4bec283404a1ba10b0235fea646f34269cb8bb97
Meldo Doka <145338717+astreonharmen@users.noreply.github.com>
2025-11-28 15:39:52 +0700

    portofolio
    about me

---
+++ about.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+
+<link rel="stylesheet" href="css/utama.css">
+<html lang="id">
+<?php require "components/head.php"; ?>
+<body>
+<?php require "components/navbar.php"; ?>
+<?php require "pages/page-about.php"; ?>
+</body>
+<script>
+  // mengambil URL path sekarang
+  const currentPath = window.location.pathname;
+
+  // ambil semua link dalam navbar
+  const navLinks = document.querySelectorAll("nav a");
+
+  navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("active");
+    }
+  });
+</script>
+</html>
